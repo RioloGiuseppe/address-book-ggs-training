fcadbbe20abc5e5ef88fd803d8f74466dd917c6a
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2020-06-18 16:21:22 +0200

    added lambda and ext method explanation

---
+++ address-book-ggs-training/Extension/LinqExplain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace address_book_ggs_training.Extension
+{
+    public static class LinqExplain
+    {
+        public static T IlPrimoOIlPredefinito<T>(this IList<T> contacts, Func<T, bool> predicate)
+        {
+            for (var i = 0; i < contacts.Count; i++)
+            {
+                if (predicate(contacts[i]))
+                {
+                    return contacts[i];
+                }
+            }
+            return default;
+        }
+
+        public static T IlPrimo<T>(this IList<T> contacts, Func<T, bool> predicate)
+        {
+            for (var i = 0; i < contacts.Count; i++)
+            {
+                if (predicate(contacts[i]))
+                {
+                    return contacts[i];
+                }
+            }
+            throw new Exception();
+        }
+    }
+}
