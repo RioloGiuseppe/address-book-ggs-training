84a2b9a5e618e9f53b9991baec79990a07f21d0f
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2020-06-26 18:51:26 +0200

    mongo sample

---
+++ mongo-db-driver-sample/MongoDbContext.cs
@@ -0,0 +1,49 @@
+﻿using mongo_db_driver_sample.Collections;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mongo_db_driver_sample
+{
+    public class MongoDbContext
+    {
+        protected MongoClient Client { get; set; }
+        public IMongoDatabase Database { get; protected set; }
+        public IMongoCollection<Contact> Contacts { get; protected set; }
+
+        public MongoDbContext(string connectionString)
+        {
+            Client = new MongoClient(connectionString);
+        }
+
+        public async virtual Task InitDb()
+        {
+            // get or create db
+            Database = Client.GetDatabase("demo");
+
+            // List databases on server
+            Console.WriteLine("Databases:");
+            var t = await Client.ListDatabasesAsync();
+            await t.ForEachAsync(db => Console.WriteLine($"  {db["name"]}"));
+            Console.WriteLine();
+
+            // List databases on server
+            Console.WriteLine("Collections:");
+            await (await Database.ListCollectionsAsync()).ForEachAsync(db => Console.WriteLine($"  {db["name"]}"));
+            Console.WriteLine();
+
+            Contacts = Database.GetCollection<Contact>("Contacts");
+
+            Console.WriteLine("Contacts:");
+            var contacts = await Contacts.Find(new BsonDocument()).ToListAsync();
+            foreach (var c in contacts)
+            {
+                Console.WriteLine($"  {c.Name} {c.LastName}");
+            }
+        }
+    }
+}
---
+++ mongo-db-driver-sample/Program.cs
@@ -0,0 +1,30 @@
+﻿using mongo_db_driver_sample.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace mongo_db_driver_sample
+{
+    class Program
+    {
+        static AutoResetEvent are = new AutoResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(async () =>
+            {
+                var client = new MongoDbContext("mongodb://localhost:27017/?readPreference=primary&ssl=false");
+                await client.InitDb();
+                await client.Contacts.InsertOneAsync(new Contact()
+                {
+                    Name = "Giovanni",
+                    LastName = "Rossi"
+                });
+            }, TaskCreationOptions.LongRunning);
+            are.WaitOne();
+        }
+    }
+}
