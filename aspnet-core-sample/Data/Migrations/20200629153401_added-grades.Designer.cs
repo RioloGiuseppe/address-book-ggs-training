f2996dba2e166eef38c819b609331f058f9009ae
Giuseppe Riolo <riolo.giuseppe@gmail.com>
2020-06-29 18:11:12 +0200

    added migration to support students and grades

--- aspnet-core-sample/Data/Migrations/20200629153401_added-grades.Designer.cs
+++ aspnet-core-sample/Data/Migrations/20200629153401_added-grades.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using aspnet_core_sample.Data;
 
 namespace aspnet_core_sample.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200629153401_added-grades")]
+    partial class addedgrades
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
